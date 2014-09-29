aff151f1a68762a8278f1dc509b1ebeeaf45cf85
Fernando Daciuk <f.daciuk@gmail.com>
2014-09-29 08:30:40 -0300

    Update boot.js

--- src/js/boot.js
+++ src/js/boot.js
@@ -35,38 +35,3 @@
     });
 
 })( window, document );
-
-
-
-
-
-
-
-
-
-
-// ;(function( window ) {
-//     'use strict';
-
-//     var getLocation = function getLocation() {
-//         var url = window.location.href;
-//         if( url.indexOf( 'index.html' ) > -1 ) {
-//             return url.split( '/index.html' ).join( '' );
-//         }
-
-//         return '.';
-//     };
-
-//     requirejs.config({
-//         baseUrl: getLocation() + '/public/js',
-//         paths : {
-//             react          : 'vendor/react.min',
-//             modules        : 'modules',
-//             components     : 'components'
-//         }
-//     });
-
-//     requirejs([ 'react' ], function() {
-//         requirejs([ 'components/MainComponent' ]);
-//     });
-// })( window );
